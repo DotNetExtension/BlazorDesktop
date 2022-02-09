ec7e97ad762499e0c2e9e6b26a20353906934e7a
Andrew Babbitt <andrew.babbitt@outlook.com>
2022-02-08 22:49:03 -0700

    added templates

---
+++ src/BlazorDesktop.Templates/templates/BlazorDesktop-CSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using BlazorDesktop_CSharp;
+using BlazorDesktop_CSharp.Data;
+using BlazorDesktop.Hosting;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = BlazorDesktopHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddSingleton<WeatherForecastService>();
+
+await builder.Build().RunAsync();
